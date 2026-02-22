ec4d454e78a9101edc1507f95e73fd30fd7db482
Marik0023 <70592085+Marik0023@users.noreply.github.com>
2026-02-23 00:34:03 +0400

    Add files via upload

---
+++ src/data/defaults.js
@@ -0,0 +1,21 @@
+export const DEFAULT_PRESET = {
+  base: "soft_capsule",
+  skinTone: "peach",
+  hair: "wave_short",
+  eyes: "oval_smile",
+  brows: "soft",
+  mouth: "smile",
+  outfit: "hoodie",
+  accessories: {
+    glasses: null,
+    hat: null,
+    neck: null,
+    hand: null,
+  },
+  box: {
+    theme: "midnight",
+    title: "MARKO",
+    subtitle: "Custom Figure",
+  },
+  bg: "studio_dark",
+};
